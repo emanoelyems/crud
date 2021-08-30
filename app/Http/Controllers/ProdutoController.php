e975fc308983d8c721dd49ebb534a6bf97ce5706
Emanoely  Santos <emanoelyems@gmail.com>
2021-08-30 13:19:20 -0300

    deletar

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,9 +1,10 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
-
+ 
 class ProdutoController extends Controller
 {
     /**
@@ -13,9 +14,14 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        //
+        // listar todos os produtos
+         $produtos = Produto::orderBy('nome', 'ASC')->get();
+        
+         //dd($produtos);
+         return view('produto.index', ['produtos' => $produtos]);
+ 
     }
-
+ 
     /**
      * Show the form for creating a new resource.
      *
@@ -23,9 +29,10 @@ class ProdutoController extends Controller
      */
     public function create()
     {
-        //
+        return view('produto.create');
+ 
     }
-
+ 
     /**
      * Store a newly created resource in storage.
      *
@@ -34,9 +41,31 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
+ 
         //
+ 
+        //$data = $request->all();
+        // dd($data);
+        $message = [
+            'nome.required' => 'O campo nome é obrigatório!',
+            'nome.min' => 'O campo nome precisa ter no mínimo :min caracteres!',
+            'descricao.required' => 'O campo descricao é obrigatório!', 
+        ];
+ 
+        $validateData = $request->validate([
+            'nome'      => 'required|min:4',
+            'descricao' =>  'required',
+         ], $message);
+ 
+        $produto = new Produto;
+        $produto->nome = $request->nome;
+        $produto->descricao = $request->descricao;
+        $produto->save();
+ 
+        return redirect()->route('produto.index')->with('message', 'Produto criado com sucesso!');
+ 
     }
-
+ 
     /**
      * Display the specified resource.
      *
@@ -46,8 +75,12 @@ class ProdutoController extends Controller
     public function show($id)
     {
         //
+ 
+        $produto = Produto::findOrFail($id);
+        // dd($produto);
+        return view('produto.show', ['produto' => $produto]);
     }
-
+ 
     /**
      * Show the form for editing the specified resource.
      *
@@ -56,9 +89,11 @@ class ProdutoController extends Controller
      */
     public function edit($id)
     {
-        //
+        // dd("Editar o registro {id}");
+        $produto = Produto::findOrFail($id);
+        return view('produto.edit', ['produto' => $produto]);
     }
-
+ 
     /**
      * Update the specified resource in storage.
      *
@@ -69,8 +104,26 @@ class ProdutoController extends Controller
     public function update(Request $request, $id)
     {
         //
+ 
+        $message = [
+            'nome.required' => 'O campo nome é obrigatório!',
+            'nome.min' => 'O campo nome precisa ter no mínimo :min caracteres!',
+            'descricao.required' => 'O campo descricao é obrigatório!', 
+        ];
+ 
+        $validateData = $request->validate([
+            'nome'      => 'required|min:4',
+            'descricao' =>  'required',
+         ], $message);
+ 
+        $produto = Produto::findOrFail($id);
+        $produto->nome = $request->nome;
+        $produto->descricao = $request->descricao;
+        $produto->save();
+ 
+        return redirect()->route('produto.index')->with('message', 'Produto editado com sucesso!');
     }
-
+ 
     /**
      * Remove the specified resource from storage.
      *
@@ -79,6 +132,10 @@ class ProdutoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // dd('DESTROY');
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+ 
+        return redirect()->route('produto.index')->with('message', 'Produto excluído com sucesso!');
     }
 }
---
+++ resources/views/produto/edit.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Editar</title>
+</head>
+<body>
+    
+    
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $errors)
+                    <li> {{$errors}} </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+ 
+    {{ Form::model($produto, array('route' => array('produto.update', $produto->id), 'method' => 'PUT' )) }}
+ 
+    {{ Form::label('nome', 'Nome') }}
+    {{ Form::text('nome', $produto->nome) }}
+</br>
+ 
+    {{ Form::label('descricao', 'Descrição') }}
+    {{ Form::text('descricao', $produto->descricao) }}
+</br>
+ 
+    {{ Form::submit('Enviar') }}
+ 
+    {{ Form::close() }}
+ 
+ 
+</body>
+</html>
